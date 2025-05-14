50afb0304e0a756f6c03e875d455a994df35fd2f
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-14 18:50:40 +0200

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,7 @@
 ?>
 <footer>
     <div class="container">
-        <p>&copy; <?php echo date("Y"); ?> jocarsa | salmon. Todos los derechos reservados.</p>
+        <p>&copy; <?php echo date("Y"); ?> andrei | salmon. Todos los derechos reservados.</p>
     </div>
 </footer>
 </body>
