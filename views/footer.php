194bbe27addca7b17909ecf4edfb11775370beda
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-14 18:36:42 +0200

    Update footer.php

---
+++ views/footer.php
@@ -0,0 +1,10 @@
+<?php
+// views/footer.php
+?>
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?> jocarsa | salmon. Todos los derechos reservados.</p>
+    </div>
+</footer>
+</body>
+</html>
